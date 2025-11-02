d1ef85cb8b707d4ffc4524636b93da8b71c60a66
Juanjo Torroglosa <jjtorroglosa@gmail.com>
2025-11-02 23:32:12 +0100

    Print tokens

--- cmd/nasheets/main.go
+++ cmd/nasheets/main.go
@@ -65,14 +65,7 @@ func main() {
 
 		if *printTokens {
 			lexer := internal.NewLexer(string(data))
-			_, err := lexer.Lex()
-			if err != nil {
-				log.Fatalf("lexer error: %v", err)
-			}
-
-			// for _, t := range tokens {
-			// 	fmt.Printf("%s: %s\n", t.Type, t.Value)
-			// }
+			lexer.PrintTokens()
 		}
 
 		if *printSong {
